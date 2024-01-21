bb98af85f55c17465807ed1f10e85b9a41073a98
Naman Vaishnav <namanmvaishnav@gmail.com>
2024-01-21 12:25:27 +0530

    feat: add setting up root VC UIKit/SwiftUI

---
+++ CoordinatorProject/ContetView.swift
@@ -0,0 +1,18 @@
+//
+//  ContetView.swift
+//  CoordinatorProject
+//
+//  Created by Naman Vaishnav on 21/01/24.
+//
+
+import SwiftUI
+
+struct ContetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContetView()
+}
